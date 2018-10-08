d020bb7db34dabedb3935d8af744d79a2048a556
birendra <33861012+bire0011@users.noreply.github.com>
2018-10-08 12:55:49 +0200

    Add project files.

---
+++ TcpServers/Conversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using DLLWeightConversion;
+
+namespace TcpServers
+{
+    class Conversion
+    {
+        public static void DoIt(TcpClient Client)
+        { 
+            Class1 Converter= new Class1();
+            NetworkStream Stream = Client.GetStream();
+            StreamReader Reader = new StreamReader(Stream);
+            StreamWriter Writer = new StreamWriter(Stream) { AutoFlush = true };
+            while (true)
+            {
+                string request = Reader.ReadLine();
+                string[] Words = request.Split(' ');
+                switch (Words[10])
+                {
+                    case "ToGrams":
+                        double number1 = double.Parse(Words[1]);
+                        number1 = Converter.ConvertToGrams(number1);
+                        Writer.WriteLine(number1);
+                        Writer.Flush();
+                        break;
+                    case "ToOunces":
+                        var number2 = double.Parse(Words[1]);
+                        number2 = Converter.ConvertToOunces(number2);
+                        Writer.WriteLine(number2);
+                        Writer.Flush();
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
+
---
+++ TcpServers/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServers
+{
+    class Program
+    {
+            private static readonly int PORT = 7070;
+        static void Main(string[] args)
+        {
+            IPAddress localAddress = IPAddress.Loopback;
+            // ip and port of server
+            TcpListener serverSocket = new TcpListener(localAddress, PORT);
+            //starting server
+            serverSocket.Start();
+            Console.WriteLine("TCP Server running on port " + PORT);
+
+            while (true) // server loop keeps it running
+            {
+                try
+                {
+                    // waiting for incoming client
+                    TcpClient client = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("incoming client");
+
+                    //allows multiple clients
+                    //Task.Run(() => PerformConversion.DoIt(client));
+
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("exception. it will continue");
+
+
+                }
+            }
+        }
+    }
+}
+   
+
+
